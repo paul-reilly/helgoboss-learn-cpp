f03ffb19f6b290e82e88ab1665df6fd4f9b83b1e
Paul Reilly <paul-reilly@users.noreply.github.com>
2023-02-23 18:04:55 +0000

    Remove old build files

--- cmake/helgoboss-learn-config.cmake
+++
@@ -1,5 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(helgoboss-midi 0.1.0 CONFIG REQUIRED)
-find_dependency(rxcpp CONFIG REQUIRED)
-find_dependency(wdl-eel2 CONFIG REQUIRED)
-include("${CMAKE_CURRENT_LIST_DIR}/helgoboss-learn-targets.cmake")
